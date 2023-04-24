546eb1740724f4f426c0ecd1db19bc8f756ec3ea
ChristopherCriollo <718675@student.dusd.net>
2023-04-24 09:53:19 -0700

    5.3

--- Unit5CriolloP1/Assets/Scripts/Target.cs
+++ Unit5CriolloP1/Assets/Scripts/Target.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+   
+    private GameManager gameManager;
     private Rigidbody targetRb;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
-
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -28,6 +33,20 @@ public class Target : MonoBehaviour
 
     }
 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+        
+    }
+
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
